4b0b61d1fec1c65a4065049c5ab51c413d083195
Skaduvee <2093671@students.wits.ac.za>
2022-07-04 01:45:21 +0200

    Added animations to all the pages

--- JavaScript/animations.js
+++ JavaScript/animations.js
@@ -1,19 +1,28 @@
 const pageLogo = document.querySelector(".logo");
 const navLinks = document.querySelectorAll(".u-url.navlink");
 
-anime({
-    targets: pageLogo,
-    translateX: 200,
-    duration: 2000,
-    direction: "reverse",
-    easing: "easeInOutSine"
-});
+function NavBarAnimations() {
+    anime({
+        targets: pageLogo,
+        translateX: 200,
+        duration: 1000,
+        scale: 0.5,
+        direction: "reverse",
+        easing: "easeInOutSine"
+    });
+    
+    anime({
+        targets: navLinks,
+        translateX: 2,
+        scale: 0.5,
+        duration: 1000,
+        direction: "reverse",
+        easing: "easeInOutSine"
+    })
+}
 
-anime({
-    targets: navLinks,
-    translateX: 2,
-    scale: 1.5,
-    duration: 2000,
-    direction: "reverse",
-    easing: "easeInOutSine"
-})
+function RunApp() {
+    NavBarAnimations();
+}
+
+RunApp();
